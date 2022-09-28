caf381cf9aa32329979b0b39a0110104d380a3d1
Maarten Coppens <martinuske@gmail.com>
2022-09-28 11:48:11 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,180 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const commandExists = require('command-exists').sync;
+const os = require('os');
+const { readFile, writeFile } = require('fs/promises');
+
+const DEFAULT_REPOSITORY_URL =
+  'https://github.com/Capitalisk/capitalisk-core.git';
+const DEFAULT_DIR_NAME = 'capitalisk-core';
+const DEFAULT_NETWORK_SYMBOL = 'clsk';
+const JSON_INDENTATION = 2;
+
+class CustomError extends Error {
+  constructor(message, name) {
+    this.name = name;
+    this.message = message;
+  }
+}
+
+class CapitaliskDeployer {
+  #deployed;
+
+  constructor({
+    gitUrl = DEFAULT_REPOSITORY_URL,
+    dirName = DEFAULT_DIR_NAME,
+    networkSymbol = DEFAULT_NETWORK_SYMBOL,
+    path = process.cwd(),
+  }) {
+    this.gitUrl = gitUrl;
+    this.dirName = dirName;
+    this.networkSymbol = networkSymbol;
+    this.path = path;
+    this.#deployed = false;
+
+    if (os.platform().indexOf('win') > -1) {
+      throw new CustomError('Not supported for Windows, yet.', 'WindowsError');
+    }
+
+    if (!commandExists('git')) {
+      throw new CustomError(
+        'You need to install git for this to work. Run sudo apt install git',
+        'GitNotInstalled',
+      );
+    }
+
+    if (!commandExists('docker-compose')) {
+      throw new CustomError(
+        'You need to install docker-compose for this to work.\nSee how on https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository.',
+        'DockerNotInstalled',
+      );
+    }
+
+    this.#verifyDeployment();
+  }
+
+  async #verifyDeployment() {
+    const { stdout } = await exec('docker ps');
+    this.#deployed = !!stdout.match(/capitalisk-core/);
+  }
+
+  async deploy() {
+    console.log(`Cloning ${this.gitUrl} as ${this.dirName}`);
+    await exec(`git clone ${this.gitUrl} ${this.dirName}`, {
+      cwd: `${this.path}`,
+    });
+
+    console.log(`Build docker`);
+    await exec(`docker-compose build --no-cache`, {
+      cwd: `${this.path}/${this.dirName}`,
+    });
+
+    console.log(`Spin up container`);
+    await exec(`docker-compose up -d`, {
+      cwd: `${this.path}/${this.dirName}`,
+    });
+
+    this.#deployed = true;
+  }
+
+  async undeploy() {
+    if (!this.#deployed) {
+      throw new CustomError(
+        'Unable to run undeploy. No container is deployed!',
+        'NoDeployFound',
+      );
+    }
+
+    console.log(`Shutting down container`);
+    try {
+      await exec(`docker-compose down`, {
+        cwd: `${this.path}/${this.dirName}`,
+      });
+    } catch (e) {
+      throw new CustomError(`Container not found`, 'ContainerNotFound');
+    }
+
+    this.#deployed = false;
+  }
+
+  async updateDeploy() {
+    console.log(`Recreating docker containers`);
+    await exec(`docker-compose up -d --force-recreate`);
+  }
+
+  async createGenesis(genesis) {
+    try {
+      const g = await this.#readJSONFile(
+        `${this.path}/${this.dirName}/genesis/mainnet/${this.networkSymbol}-genesis.json`,
+      );
+
+      if (g && g.networkSymbol === this.networkSymbol) {
+        console.log('Genesis is already present');
+      }
+    } catch (e) {}
+
+    try {
+      await this.#writeJSONFile(
+        `${this.path}/${this.dirName}/genesis/mainnet/${this.networkSymbol}-genesis.json`,
+        genesis,
+      );
+    } catch (e) {
+      throw new CustomError(`Error writing genesis file! ${e.message}`, 'GenesisWriteFail');
+    }
+  }
+
+  async addConfig(config, extendCapitalisk) {
+    if (extendCapitalisk) {
+      console.log(`Adding config`);
+      const c = await this.#readJSONFile(
+        `${this.path}/${this.dirName}/config.json`,
+      );
+
+      console.log(`Adding database inside postgresql container`);
+      try {
+        await exec(
+          `docker exec capitalisk-postgres runuser -l postgres -c "createdb -U ldpos ${config.components.dal.connection.database}"`,
+        );
+      } catch (e) {
+        if (e.message.match(/exists/g)) {
+          console.log('Recreating the database');
+          await exec(
+            `docker exec capitalisk-postgres runuser -l postgres -c "dropdb -U ldpos ${config.components.dal.connection.database}"`,
+          );
+          await exec(
+            `docker exec capitalisk-postgres runuser -l postgres -c "createdb -U ldpos ${config.components.dal.connection.database}"`,
+          );
+        } else {
+          throw new CustomError(
+            `Failed to create the database in the docker container! ${e.message}`,
+            'PostgresInitFail',
+          );
+        }
+      }
+
+      c.modules[`${this.dirName.split('-')[0]}_chain`] = config;
+
+      await this.#writeJSONFile(`${this.path}/${this.dirName}/config.json`, c);
+
+      console.log(c);
+    }
+  }
+
+  async #readJSONFile(filePath) {
+    let content = await readFile(filePath, { encoding: 'utf8' });
+    try {
+      return JSON.parse(content);
+    } catch (error) {
+      throw new Error(
+        `Failed to parse the JSON content of file at path ${filePath} because of error: ${error.message}`,
+      );
+    }
+  }
+
+  async #writeJSONFile(filePath, object) {
+    let jsonString = JSON.stringify(object, null, JSON_INDENTATION);
+    return writeFile(filePath, jsonString, { encoding: 'utf8' });
+  }
+}
+
+module.exports = CapitaliskDeployer;
---
+++ test.js
@@ -0,0 +1,60 @@
+const CapitaliskDeployer = require('./index');
+
+const deployer = new CapitaliskDeployer({
+  dirName: 'ldpos-core',
+  networkSymbol: 'ldpos',
+});
+
+(async () => {
+  // await deployer.undeploy();
+  // await deployer.deploy();
+  // await deployer.addConfig();
+  // await deployer.createGenesis({
+  //   networkSymbol: 'ldpos',
+  //   accounts: [
+  //     {
+  //       address: 'ldpos71bcc5cd9c8cf5dc2c79d235ed5f2393b5ad56cb',
+  //       type: 'sig',
+  //       forgingPublicKey:
+  //         '218a928a7c8c21f7820d3bb198e41d5175886891334f4cb900062a5acb880458',
+  //       nextForgingKeyIndex: 0,
+  //       multisigPublicKey:
+  //         '4826718943ce648ce1999549179f85df45a2477c6a51f07c656c6dc18b6a9ddf',
+  //       nextMultisigKeyIndex: 0,
+  //       sigPublicKey:
+  //         '71bcc5cd9c8cf5dc2c79d235ed5f2393b5ad56cb9b3f4b0102e7e32c804c0a5a',
+  //       nextSigKeyIndex: 0,
+  //       balance: '10000000000000000',
+  //       votes: ['ldpos71bcc5cd9c8cf5dc2c79d235ed5f2393b5ad56cb'],
+  //     },
+  //   ],
+  // });
+  // await deployer.addConfig(
+  //   {
+  //     modulePath: 'node_modules/ldpos-chain',
+  //     genesisPath: 'genesis/mainnet/ldpos-genesis.json',
+  //     components: {
+  //       logger: {
+  //         logFileName: 'logs/mainnet/ldpos.log',
+  //         consoleLogLevel: 'debug',
+  //         fileLogLevel: 'error',
+  //       },
+  //       dal: {
+  //         libPath: 'node_modules/ldpos-pg-dal',
+  //         client: 'pg',
+  //         connection: {
+  //           host: '127.0.0.1',
+  //           user: 'ldpos',
+  //           password: 'ldpos',
+  //           database: 'ldpos_main',
+  //           port: '5432',
+  //         },
+  //       },
+  //     },
+  //   },
+  //   true,
+  // );
+
+  // await deployer.verifyDeployment();
+  // console.log(deployer.deployed)
+})();
